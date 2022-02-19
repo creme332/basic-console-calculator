55436d46526a564064b60bc899d272c8ab9d26f8
creme332 <65414576+creme332@users.noreply.github.com>
2022-02-19 13:19:11 +0400

    initial phase

---
+++ Calculator.cpp
@@ -0,0 +1,201 @@
+#include <string>
+#include <iostream>
+#include <regex>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+int precedence(char op) {
+    switch (op) {
+    case '-':return 2;
+    case '+':return 2;
+    case '/':return 3;
+    case '*':return 3;
+    case '^':return 4;
+    case 'm': return 5; //unary minus
+    case 'p': return 5; //unary plus
+    }
+    return -1;
+}
+char associativity(char op) {
+    //L : left associativity R: right associativity
+    switch (op) {
+    case '-':return 'L';
+    case '+':return 'L';
+    case '/':return 'L';
+    case '*':return 'L';
+    case '^':return 'R';
+    case 'm':return 'R'; //unary minus
+    case 'p':return 'R'; //unary plus
+    }
+    return '.';
+}
+
+bool isoperator(char op) {
+    //used to identify unary operators. A unary operator appears just after the following op.
+    switch (op) {
+    case '-':return 1;
+    case '+':return 1;
+    case '/':return 1;
+    case '*':return 1;
+    case '^':return 1;
+    case '(':return 1;
+    }
+    //Note a minus/plus following ) is not a unary minus/plus
+    return 0;
+}
+std::string modify(std::string s) {
+    //Remove all spaces from s
+    //Replace unary plus with p
+    //Replace unary minus with m
+    //add * before ( if there's no operator
+    
+    s = std::regex_replace(s, std::regex(" "), ""); //remove spaces
+
+     //Replace unary plus with p
+    //Replace unary minus with m
+    for (long long i = 1;i < s.length();i++) {
+        if (isoperator(s[i - 1]) && (s[i] == '-' || s[i] == 'm')) {
+            s[i] = 'm';
+        }
+        if (isoperator(s[i - 1]) && (s[i] == '+' || s[i] == 'p')) {
+            s[i] = 'p';
+        }
+    }
+    if (s[0] == '-') { s[0] = 'm'; }
+    if (s[0] == '+') { s[0] = 'p'; }
+
+    //add * before ( if there's no operator
+    std::string ans="";
+    for (int i = 0;i < s.length()-1;i++) {
+        if (s[i] >= '0' && s[i] <= '9' && s[i + 1] == '(') {
+            ans += s[i]; ans+= '*';
+        }
+        else {
+            ans += s[i];
+        }
+    }
+    ans += s[s.length() - 1];
+    return ans;
+}
+
+std::string to_postfix(std::string exp) {
+    //Implementation of shunting yard algorithm.
+    exp = modify(exp);
+    std::vector <char> operators;
+    std::string ans = "";
+    std::string operand = "";
+    char prevchar = '*';
+
+    for (long long i = 0;i < exp.length();i++) {
+
+        if (i > 0) { prevchar = exp[i - 1]; }
+        char c = exp[i];
+
+        if (isdigit(c) != 0 || c == '.') { // if c is a digit or a decimal point
+            operand += c;
+        }
+        else {// c is an operator 
+
+          //add operand to ans
+            if (operand != "") {
+                ans += operand + " ";
+                operand = "";
+            }
+            if (operators.size() == 0 || c == '(') {
+                operators.push_back(c);
+            }
+            else { //operator stack not empty
+                char top = operators.back(); //operator at top of operator stack
+                if (c == ')') {
+                    while (top != '(') {
+                        ans += top;ans += " ";
+                        operators.pop_back();
+                        top = operators.back();
+                    }
+                    operators.pop_back(); //pop the (
+                }
+                else {
+                    if (top == '(' || (associativity(c) == 'R' && (precedence(c) >= precedence(top)))) {
+                        operators.push_back(c);
+                    }
+                    else {
+                        if (associativity(c) == 'L') {
+                            while (precedence(c) <= precedence(top) && operators.size() != 0 && top != '(') {
+                                ans += top; ans += " ";
+                                operators.pop_back();
+                                if (operators.size() == 0) { break; }
+                                top = operators.back();
+                            }
+                            operators.push_back(c);
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+    if (operand != "") {
+        ans += operand + " ";
+    }
+    if (operators.size() != 0) {
+        for (int i = operators.size() - 1;i > -1;i--) {
+            ans += operators[i]; ans += " ";
+        }
+    }
+
+    return ans;
+}
+
+double result(double a, double b, std::string o) {
+    //result(5.2, 1.1, "-") returns 4.1
+    if (o == "+") { return a + b; }
+    if (o == "-") { return a - b; }
+    if (o == "*") { return a * b; }
+    if (o == "/") { return a / b; }
+    if (o == "^") { return pow(a, b); }
+}
+double EvaluateExpression(std::string exp) {
+    std::string postfix = to_postfix(exp); //postfix has spaces but no ()
+
+    std::regex regex(R"([\s]+)");
+    std::vector<std::string> out( // save all space-separated operands and operators to out
+        std::sregex_token_iterator(postfix.begin(), postfix.end(), regex, -1),
+        std::sregex_token_iterator()
+    );
+
+    //Note : unary plus is always ignored
+    std::vector <double> Operand;
+    for (auto i : out) {
+        if (i == "m") { //unary minus
+            double a = Operand.back();
+            Operand.pop_back();
+            Operand.push_back(a * -1);
+        }
+        else {
+            if (i == "*" || i == "/" || i == "+" || i == "-" || i == "^") {
+                double b = Operand.back();
+                Operand.pop_back();
+                double a = Operand.back();
+                Operand.pop_back();
+                Operand.push_back(result(a, b, i));
+            }
+            else {
+                Operand.push_back(stod(i));
+            }
+        }
+    }
+    return Operand.back();
+}
+int main() {
+    std::string expression; int p;
+    std::cout << "Enter your mathematical expression : ";
+    std::cin >> expression;
+    std::cout << "Enter precision (number of decimal places): ";
+    std::cin >> p;
+    double result;
+    std::cout << std::fixed << std::setprecision(p) << EvaluateExpression(expression)<<"\n";
+    system("pause");
+}
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # BasicCalculator
-A basic calculator that follows BODMAS/PEMDAS. It can deal with complex expressions involving nested parentheses.
+Simple console mode calculator that follows BODMAS/PEMDAS, deals with nested parentheses, unary operators, decimal numbers, and powers. Precision of calculation can be varied as well.
+
+It uses the Shunting Yard Algorithm to convert the input infix expression to postfix notation and then evaluates it.
+
+# Examples of valid input #
+
+- `"112+22*(2*(22-53^2*(2))-7/(7/2-1/4))" = -246159.38462`
+- `"2+-1" = 1`
+- `"2^0.5" = 1.413`
+- `"7^7^(2/(4-532)^3)" = 7.0`
+- `"2(2)"`  (=`"2*(2)"`)
+
+
+# Examples of invalid input #
+
+- `"1---1"`
+- `"2*"`
+- `"7 ^ 7 ^ (2/(4-532)^3)"` (No spaces are allowed when inputting directly from console)
+
+
+# Current limitations #
+- Range of numbers is limited by `double` data type.
+- No support for trigonometric and logarithmic expressions.
+- No support for expressions containing irrational numbers (eg pi, e)
+- Input is not validated so all invalid inputs will cause program to crash.
